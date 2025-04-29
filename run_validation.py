91d7db1085cbef14203b6bc48baca31a466256ed
Furkan Akkurt <cigarette.hachette35@icloud.com>
2025-04-29 12:31:26 +0300

    update parallel sentences table

--- run_validation.py
+++ run_validation.py
@@ -31,7 +31,16 @@ def main():
         os.system(f'cd {tools_dir} && git pull')
     os.chdir(script_dir)
     
-    validation_command = f'python {validation_script_path} --lang {language} --max-err 0 {' '.join([str(file) for file in conllu_files])} &> validation.log'
+    # Extract treebank title from the first file
+    treebank_pattern = re.compile(r'(\w+_\w+)-')
+    treebank_match = treebank_pattern.search(conllu_files[0].name)
+    if treebank_match:
+        treebank_title = treebank_match.group(1)
+    else:
+        treebank_title = language
+    
+    log_filename = f'{treebank_title}_validation.log'
+    validation_command = f'python {validation_script_path} --lang {language} --max-err 0 {' '.join([str(file) for file in conllu_files])} &> {log_filename}'
     os.system(validation_command)
 
 if __name__ == '__main__':
